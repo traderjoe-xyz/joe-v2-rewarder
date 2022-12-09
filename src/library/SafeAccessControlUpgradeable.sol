dfdc8ce18d6ae67a382822974c102ad0bd1cf4c5
0x0Louis <louis@joepegs.com>
2022-12-09 18:26:13 +0100

    fix gap size

--- src/library/SafeAccessControlUpgradeable.sol
+++ src/library/SafeAccessControlUpgradeable.sol
@@ -109,5 +109,5 @@ abstract contract SafeAccessControlUpgradeable is Ownable2StepUpgradeable, Acces
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[50] private __gap;
 }
